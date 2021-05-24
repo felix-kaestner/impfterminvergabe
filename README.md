81668390d0f5137df298490695d89cf4b4909901
Felix <kaestner.fe@gmail.com>
2021-05-24 18:07:04 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # impfterminvergabe
-A simple proof of concept automation for getting a vaccination appointment against COVID-19 in Saxony, Germany
+
+A simple **proof of concept** automation for getting a vaccination appointment against COVID-19 in Saxony, Germany.
+
+## Idea
+
+This project provides a simple automation build on top of [selenium](https://www.selenium.dev/) which launches a new
+Chrome window and submits the form for getting a vaccination appointment. In case you get a suggestion for an available
+appointment the automation stops and leaves you with an open Chrome window, in which case you can decide to accept or
+decline and complete the procedure manually.
+
+## Quick Start
+
+1. Generate a user account at [https://sachsen.impfterminvergabe.de/](https://sachsen.impfterminvergabe.de/).
+
+    Make sure to save the `username` ("Vorgangskennung") and `password` of your account. You might also want to generate a second user, if you want to get an appointment together with someone other.
+
+
+2. Make sure you have [Google Chrome](https://www.google.com/intl/de_de/chrome/) (or [Chromium](https://www.chromium.org/)) installed on your machine. Get the exact version number of Chrome installed on your machine. Simple click the three dots in the upper right corner of Chrome and select `Help > About`. You should find a version number like `Version 90.0.4430.212 (Offizieller Build) (64-Bit)`.
+
+
+3. Download the appropriate [Chrome Webdriver](https://sites.google.com/a/chromium.org/chromedriver/downloads) for your Chrome version and operating system. Afterwards, extract the `chromedriver` (or `chromedriver.exe`) file from the zip archive.
+
+
+4. Fill in the information inside [main.py](main.py) on line 11 - 17.
+
+    Substitute your `username` and `password` (optionally your partner account, otherwise leave as is) and specify the absolute `path` to the chromedriver executable from Step 3. 
+
+    ```python
+    ...
+    username = "Axxx-xxxxx"
+    password = "12345"
+    
+    partner_username = None
+    partner_password = None
+    
+    path = "C:\chromedriver.exe"
+    ...
+    ```
+   
+5. Create a virtual environment using [virtualenv](https://github.com/pypa/virtualenv) and install the [selenium](https://www.selenium.dev/) package.
+
+    ```bash
+    $ python3 -m venv env
+    $ source ./env/bin/activate # or .\env\Scripts\activate on Windows
+    $ pip install selenium
+    ```
+   Otherwise, just use the integration of an IDE like [PyCharm](https://www.jetbrains.com/de-de/pycharm/) to get you started.
+
+
+6. Run the script.
+
+    ```bash
+    $ python3 main.py
+    ```
+   
+_Optional_
+
+If you do not want a vaccination appointment in some venues available, simply delete the corresponding entries from the `locations` dictionary (line 24-40).
+
+## Legal Disclaimer
+
+```markdown
+Copyright © 2021 Felix Kästner
+
+Permission to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of this software and associated 
+documentation files (the “Software”) is hereby not granted to 
+any person.
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+```
+
---
+++ main.py
@@ -0,0 +1,131 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+
+from time import sleep
+
+timeout = 30  # seconds
+
+username = "Axxx-xxxxx"
+password = "12345"
+
+partner_username = None
+partner_password = None
+
+path = "C:\chromedriver.exe"
+
+registration_url = "https://sachsen.impfterminvergabe.de/civ.public/start.html?oe=00.00.IM&mode=cc&cc_key=IOAktion"
+
+driver = webdriver.Chrome(path)
+driver.get(registration_url)
+
+locations = {
+    "BG01": "Stadthalle Belgern, Mühlberger Str. 37, 04874 Belgern-Schildau",
+    "BN01": "ehem. Aldi Markt Borna, Oststraße 3a, 04552 Borna",
+    "BZ01": "Sporthalle am Flughafen, Macherstraße 146, 01917 Kamenz",
+    "CN01": "Richard-Hartmann-Halle, Fabrikstraße 9, 09111 Chemnitz",
+    "DD01": "Messe Dresden, Messering 6, 01067 Dresden",
+    "EC01": "Spektrum Treuen/Eich (ehem. Baumarkt), Rebesgrüner Str. 9, 08223 Treuen OT Eich",
+    "EZ01": "Festhalle Annaberg-Buchholz, Ernst-Roch-Straße 4, 09456 Annaberg-Buchholz",
+    "GR01": "TIZ Grimma, Muldentalhalle, Südstraße 80, 04668 Grimma",
+    "LB01": "Messehalle Löbau, Görlitzer Str. 2, 02708 Löbau",
+    "LZ01": "Messe Leipzig, Messe-Allee 1, 04356 Leipzig",
+    "MW01": "Mittweida über Simmel-Markt (ehem. EKZ), Schillerstraße 1, 09648 Mittweida",
+    "PL01": "IZ Plauen, Europaratstraße 5, 08523 Plauen",
+    "PN01": "Aldi Pirna Jessen, Radeberger Str. 1h, 01796 Pirna",
+    "RI01": "Sachsen-Arena Riesa, Am Sportzentrum 5, 01589 Riesa",
+    "ZW01": "Stadthalle Zwickau, Bergmannsstraße 1, 08056 Zwickau"
+}
+
+
+def get_element(locator):
+    return WebDriverWait(driver, timeout).until(expected_conditions.presence_of_element_located(locator))
+
+
+def navigate_next():
+    driver.find_element_by_id("WorkflowButton-4212").click()
+
+
+def navigate_back():
+    driver.find_element_by_id("WorkflowButton-4255").click()
+
+
+def page_1():
+    username_input = get_element((By.ID, "gwt-uid-3"))
+    password_input = get_element((By.ID, "gwt-uid-5"))
+
+    username_input.send_keys(username)
+    password_input.send_keys(password)
+
+    navigate_next()
+
+
+def page_2():
+    option_label = get_element((By.CSS_SELECTOR, "#gwt-uid-9 + label"))
+    option_label.click()
+
+    navigate_next()
+
+
+def page_3():
+    if partner_username is not None and partner_password is not None and username != partner_username and password != partner_password:
+        fill_partner()
+
+    while True:
+        for value, name in locations.items():
+            print(f"Try location: {name}")
+            query_location(value, name)
+
+
+def fill_partner():
+    option_label = get_element((By.CSS_SELECTOR, "#gwt-uid-44 + label"))
+    option_label.click()
+
+    username_input = get_element((By.ID, "gwt-uid-55"))
+    password_input = get_element((By.ID, "gwt-uid-57"))
+
+    username_input.send_keys(partner_username)
+    password_input.send_keys(partner_password)
+
+
+def open_location_dropdown():
+    select = get_element((By.CSS_SELECTOR, ".select2-selection"))
+    driver.execute_script("window.scrollTo(0, 600);")
+
+    action = ActionChains(driver)
+    action.move_to_element_with_offset(select, 5, 5)
+    action.click()
+    action.perform()
+
+
+def query_location(value, name):
+    open_location_dropdown()
+
+    option = get_element((By.CSS_SELECTOR, f"li[id$=\"{value}\"]"))
+    driver.execute_script("arguments[0].scrollIntoView(true);", option)
+    option.click()
+
+    navigate_next()
+
+    # noinspection PyBroadException
+    try:
+        get_element((By.XPATH, '//*[text() = "Aufgrund der aktuellen Auslastung der Impfzentren und der verfügbaren '
+                               'Impfstoffmenge können wir Ihnen leider keinen Termin anbieten. Bitte versuchen Sie es '
+                               'in ein paar Tagen erneut."]'))
+        print(f"    No appointments at: {name}")
+        navigate_back()
+    except:
+        print(f"    Open appointments at: {name}")
+        sleep(60 * 24)
+
+
+def main():
+    page_1()
+    page_2()
+    page_3()
+
+
+if __name__ == '__main__':
+    main()
